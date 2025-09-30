e4137bc8b50ea4ff91f3d1f1a2e9104fa2d7f376
cosmosZhou <744984949@qq.com>
2025-10-01 07:39:45 +0800

    new file:   Lemma/Algebra/Floor/gt/Sub_1.lean

---
+++ Lemma/Algebra/Le/of/Ge/Le.lean
@@ -0,0 +1,19 @@
+import sympy.Basic
+import Lemma.Algebra.Le.of.Le.Le
+open Algebra
+
+
+@[main]
+private lemma main
+  [Preorder α]
+  {x a b : α}
+-- given
+  (h₀ : x ≥ a)
+  (h₁ : x ≤ b) :
+-- imply
+  a ≤ b := 
+-- proof
+  Le.of.Le.Le h₀ h₁
+
+
+-- created on 2025-09-30
