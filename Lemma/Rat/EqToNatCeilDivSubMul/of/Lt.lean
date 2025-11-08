fe9d13c3e7e927ce4397c06a41cf9c06b7f84540
cosmosZhou <744984949@qq.com>
2025-11-08 18:18:00 +0800

    simp [EqCeilDivSubMul.of.Lt h]

--- Lemma/Rat/EqToNatCeilDivSubMul/of/Lt.lean
+++ Lemma/Rat/EqToNatCeilDivSubMul/of/Lt.lean
@@ -1,8 +1,5 @@
-import Lemma.Nat.CeilSub.eq.Sub_Floor
-import Lemma.Nat.Ne_0.of.Gt
-import Lemma.Rat.FloorDiv.eq.Zero.of.Lt
-import Lemma.Rat.Sub_Div.eq.DivSubMul.of.Ne_0
-open Nat Rat
+import Lemma.Rat.EqCeilDivSubMul.of.Lt
+open Rat
 
 
 @[main]
@@ -14,13 +11,7 @@ private lemma main
 -- imply
   ⌈(m * n - j : α) / n⌉.toNat = m := by
 -- proof
-  rw [DivSubMul.eq.Sub_Div.of.Ne_0]
-  ·
-    rw [CeilSub.eq.Sub_Floor]
-    simp [FloorDiv.eq.Zero.of.Lt h]
-  ·
-    simp
-    apply Ne_0.of.Gt h
+  simp [EqCeilDivSubMul.of.Lt h]
 
 
 -- created on 2025-11-08
--- Lemma/Vector/GetIndices/eq/AddMul.lean
+++ Lemma/Vector/GetIndices/eq/AddMul.lean
@@ -1,21 +1,22 @@
-import Lemma.List.GetSlicedIndices.eq.AddMul.of.Lt_Length.Gt_0.Le.Lt
 import Lemma.Bool.OrOr.is.Or_Or
 import Lemma.Int.EqToNat
+import Lemma.List.GetSlicedIndices.eq.AddMul.of.Lt_Length.Gt_0.Le.Lt
+import Lemma.List.LengthSlicedIndices.eq.ToNatCeilDivSub.of.Gt_0.Le.Lt
 import Lemma.Nat.CoeAdd.eq.AddCoeS
+import Lemma.Nat.CoeAdd_1.eq.AddCoe_1
 import Lemma.Nat.CoeMul.eq.MulCoeS
 import Lemma.Nat.EqAdd_Mul_DivSub1Sign_2
-import Lemma.Nat.EqMin
-import Lemma.Nat.Eq_Mk.of.EqVal
 import Lemma.Nat.Gt_0
 import Lemma.Nat.LeCoeS.is.Le
-import Lemma.Nat.LtCoeS.is.Lt
 import Lemma.Nat.LtVal
 import Lemma.Nat.Mul.gt.Zero.of.Gt_0.Gt_0
 import Lemma.Nat.OfNat.eq.Cast
+import Lemma.Rat.EqCeilDivSubMul.of.Lt
+import Lemma.Vector.EqGetS
 import Lemma.Vector.EqLengthSlice.of.Lt
 import sympy.core.relational
 import sympy.vector.vector
-open Bool Int Nat Vector List
+open Bool Int List Nat Rat Vector
 
 
 @[main]
@@ -60,27 +61,16 @@ private lemma main
     | 0 =>
       contradiction
     | n + 1 =>
-      denote h_n' : n' = n + 1
-      simp -- [← h_n']
       denote h_start_eq : start = (Slice.Add_Mul_DivSub1Sign_2 (m * (n + 1)) ↑↑j).toNat
       denote h_stop_eq : stop = (Slice.Add_Mul_DivSub1Sign_2 (m * (n + 1)) (↑m * (↑n + 1))).toNat.min (m * (n + 1))
       simp [← h_start_eq, ← h_stop_eq]
       rw [EqAdd_Mul_DivSub1Sign_2, EqToNat] at h_start_eq
-      rw [OfNat.eq.Cast (α := ℤ), AddCoeS.eq.CoeAdd, MulCoeS.eq.CoeMul, EqAdd_Mul_DivSub1Sign_2, EqToNat] at h_stop_eq
+      rw [OfNat.eq.Cast (α := ℤ), @Nat.AddCoeS.eq.CoeAdd, MulCoeS.eq.CoeMul, EqAdd_Mul_DivSub1Sign_2, EqToNat] at h_stop_eq
       simp at h_stop_eq
       denote h_step_eq : step = n.succ
-      let ⟨h_lt, h_pos⟩ := h
-      have h_start : start < stop := by simpa [h_start_eq, h_stop_eq]
-      have h_stop : stop ≤ m * (n + 1) := by simp [stop]
-      have h_step : step > 0 := by simp [step]
       simp [h_start_eq, h_stop_eq]
-      have := GetSlicedIndices.eq.AddMul.of.Lt_Length.Gt_0.Le.Lt h_lt (by simp) (by simp) (j := j) (m := m) (n := n + 1) (i := i)
-        (by
-          rw [List.LengthSlicedIndices.eq.ToNatCeilDivSub.of.Gt_0.Le.Lt]
-          simp
-          sorry
-        )
-      sorry
+      simp [EqGetS]
+      apply GetSlicedIndices.eq.AddMul.of.Lt_Length.Gt_0.Le.Lt h.left (by simp) (by simp) (j := j) (m := m) (n := n + 1) (i := i)
 
 
 -- created on 2025-11-07
