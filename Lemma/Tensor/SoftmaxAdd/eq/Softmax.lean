b66331e19d31d129256f5e75b54d0d538e9607b9
cosmosZhou <744984949@qq.com>
2025-11-30 19:33:50 +0800

    repeat rw [Softmax.eq.One.of.LeLength (by omega)]

--- Lemma/Tensor/SoftmaxAdd/eq/Softmax.lean
+++ Lemma/Tensor/SoftmaxAdd/eq/Softmax.lean
@@ -1,18 +1,33 @@
+import Lemma.Tensor.DataDiv.eq.DivDataS
+import Lemma.Tensor.DataExp.eq.ExpData
+import Lemma.Tensor.DataKeepdim.eq.Cast_FlattenMapSplitAtCast_Data.of.GtLength
+import Lemma.Tensor.Eq.is.EqDataS
+import Lemma.Tensor.Softmax.eq.One.of.LeLength
+import sympy.tensor.Basic
 import sympy.tensor.functions
-import sympy.Basic
+open Tensor
 
 
 @[main]
 private lemma main
-  [Exp α]
-  -- given
+  [ExpRing α]
+-- given
   (X : Tensor α s)
   (δ : α)
   (d : ℕ) :
 -- imply
-  (X + δ).softmax d = X.softmax d := by
+  (X + (δ : Tensor α [])).softmax d = X.softmax d := by
 -- proof
-  sorry
+  if h : s.length > d then
+    unfold Tensor.softmax
+    simp
+    apply Eq.of.EqDataS
+    simp [DataDiv.eq.DivDataS]
+    simp [DataExp.eq.ExpData]
+    simp [DataKeepdim.eq.Cast_FlattenMapSplitAtCast_Data.of.GtLength h]
+    sorry
+  else
+    repeat rw [Softmax.eq.One.of.LeLength (by omega)]
 
 
 -- created on 2025-11-30
