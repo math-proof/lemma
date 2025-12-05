f0741a312d791a742db2da886b35072d726c9d16
cosmosZhou <744984949@qq.com>
2025-12-05 08:14:53 +0800

    var caller = getParameterByName('caller', 'Tensor.EqDot_GradExpect.of.Eq_Conditioned.Eq_Expect.IsFinite.IsFinite.unbiased_advantage_estimate');

--- static/components/axiomSummary.vue
+++ static/components/axiomSummary.vue
@@ -179,7 +179,7 @@ where
 limit 1`;
 			console.log(sql);
 			var data = await form_post('php/request/execute.php', {sql, resultType: 1});
-			var caller = 'Tensor.EqDot_GradExpect.of.Eq_Conditioned.Eq_Expect.IsFinite.IsFinite.unbiased_advantage_estimate';
+			var caller = getParameterByName('caller', 'Tensor.EqDot_GradExpect.of.Eq_Conditioned.Eq_Expect.IsFinite.IsFinite.unbiased_advantage_estimate');
 			data = [{caller}];
 			console.log(data);
 			data = data.map(row => row.caller);
