4f80c7743ae7188561565fa3e076c6170c3f4146
cosmosZhou <744984949@qq.com>
2026-06-14 21:18:54 +0800

    modified:   Lemma/Vector/FlattenMapRange/as/UFn_0.lean

--- Lemma/Vector/FlattenMapRange/as/UFn_0.lean
+++ Lemma/Vector/FlattenMapRange/as/UFn_0.lean
@@ -1,19 +1,31 @@
-import Lemma.Vector.FlattenMapRange.eq.Cast_UFn_0
 import Lemma.Bool.SEq.is.EqCast.of.Eq
-open Vector Bool
+import Lemma.Nat.Eq_0.of.Lt_1
+import Lemma.Fin.Eq_Fin.of.EqVal
+import Lemma.Vector.Eq.is.All_EqGetS
+import Lemma.Vector.EqFlattenMapRange.of.All_EqGetUnflatten
+import Lemma.Vector.GetCast.eq.Get.of.Eq
+import Lemma.Vector.GetUnflatten.eq.Get_AddMul
+open Nat Vector Fin Bool
 
 
 @[main]
 private lemma main
-  {u : Fin 1 → List.Vector α n} :
+  (u : Fin 1 → List.Vector α n) :
 -- imply
   ((List.Vector.range 1).map fun i => u i).flatten ≃ u 0 := by
 -- proof
-  apply SEq.of.Eq_Cast
-  ·
-    apply FlattenMapRange.eq.Cast_UFn_0
-  ·
-    simp
+  apply SEq.of.Eq_Cast (h := by simp)
+  apply EqFlattenMapRange.of.All_EqGetUnflatten
+  intro t
+  have h_t := t.isLt
+  have h_t := Eq_0.of.Lt_1 h_t
+  have h_t := Eq_Fin.of.EqVal h_t
+  simp [h_t]
+  apply Eq.of.All_EqGetS
+  intro i
+  simp only [GetElem.getElem]
+  rw [GetUnflatten.eq.Get_AddMul.fin]
+  simp [GetCast.eq.Get.of.Eq.fin]
 
 
 -- created on 2025-11-11
--- Lemma/Vector/FlattenMapRange/eq/Cast_UFn_0.lean
+++ Lemma/Vector/FlattenMapRange/eq/Cast_UFn_0.lean
@@ -1,30 +1,16 @@
-import Lemma.Nat.Eq_0.of.Lt_1
-import Lemma.Fin.Eq_Fin.of.EqVal
-import Lemma.Vector.Eq.is.All_EqGetS
-import Lemma.Vector.EqFlattenMapRange.of.All_EqGetUnflatten
-import Lemma.Vector.GetCast.eq.Get.of.Eq
-import Lemma.Vector.GetUnflatten.eq.Get_AddMul
-import sympy.vector.vector
-open Nat Vector Fin
+import Lemma.Bool.EqCast.of.SEq
+import Lemma.Vector.FlattenMapRange.as.UFn_0
+open Vector Bool
 
 
 @[main]
 private lemma main
-  {u : Fin 1 → List.Vector α n} :
+  (u : Fin 1 → List.Vector α n) :
 -- imply
-  ((List.Vector.range 1).map fun i => u i).flatten = cast (by simp) (u 0) := by
+  ((List.Vector.range 1).map fun i => u i).flatten = cast (by simp) (u 0) :=
 -- proof
-  apply EqFlattenMapRange.of.All_EqGetUnflatten
-  intro t
-  have h_t := t.isLt
-  have h_t := Eq_0.of.Lt_1 h_t
-  have h_t := Eq_Fin.of.EqVal h_t
-  simp [h_t]
-  apply Eq.of.All_EqGetS
-  intro i
-  simp only [GetElem.getElem]
-  rw [GetUnflatten.eq.Get_AddMul.fin]
-  simp [GetCast.eq.Get.of.Eq.fin]
+  Eq_Cast.of.SEq (FlattenMapRange.as.UFn_0 u)
 
 
 -- created on 2025-11-11
+-- updated on 2026-06-14
