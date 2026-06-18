78897e87cc0abd33299b8bc60cd31f00d0dfb6c0
cosmosZhou <744984949@qq.com>
2026-06-18 20:41:22 +0800

    deleted:    Lemma/Tensor/Matmul/eq/Cast_SelectBatchDot/of/Lt_Get_SubLength/GeLength_2.lean

---
+++ Lemma/Tensor/Matmul/as/SelectBatchDot/of/Lt_Get_SubLength/GeLength_2.lean
@@ -0,0 +1,59 @@
+import Lemma.Bool.SEq.is.EqCast.of.Eq
+import Lemma.List.Drop.eq.ListGet.of.GtLength_0
+import Lemma.List.Drop.eq.ListGetS.of.GeLength_2
+import Lemma.List.EqAppendS.of.Eq
+import Lemma.List.EqAppendTake__Drop
+import Lemma.List.EraseIdx.eq.Append_Drop_Add_1
+import Lemma.List.EraseIdxAppend.eq.Append_EraseIdx.of.LeLength
+import Lemma.Nat.EqAddMulDiv
+import sympy.tensor.tensor
+open List Nat Bool
+
+
+@[main, cast]
+private lemma main
+  [Mul α] [Add α] [Zero α]
+-- given
+  (h_s' : s'.length ≥ 2)
+  (h_n : n < s'[s'.length - 2])
+  (X : Tensor α [n])
+  (Y : Tensor α s') :
+-- imply
+  let batch_size := s'.take (s'.length - 2)
+  let n' := s'[s'.length - 2]
+  let k' := s'[s'.length - 1]
+  let Y' : Tensor α (batch_size ++ [n', k']) := cast
+    (by
+      congr
+      simp [batch_size, n', k']
+      conv_lhs => rw [← EqAppendTake__Drop s' (s'.length - 2)]
+      apply EqAppendS.of.Eq.left
+      apply Drop.eq.ListGetS.of.GeLength_2 h_s'
+    )
+    Y
+  let q := n' / n
+  let r := n' % n
+  let X' : Tensor α [n'] := cast
+    (by simp [q, r, EqAddMulDiv])
+    ((cast (by grind) (X.repeat q ⟨0, by simp⟩) : Tensor α [q * n]) ++ (0 : Tensor α [r]))
+  let X' := X'.broadcast ((batch_size ++ [1, n'])) (by simp)
+  X.matmul Y ≃ (X'.batch_dot Y').select ⟨s'.length - 2, by simp [batch_size]⟩ ⟨0, by grind⟩ := by
+-- proof
+  intro batch_size n' k'
+  unfold Tensor.matmul
+  apply SEq.of.Eq_Cast
+  .
+    split_ifs
+    repeat grind
+  .
+    congr
+    simp [batch_size, k', Tensor.matmul_shape]
+    have h_s' : s' ≠ [] := by grind
+    simp [h_s']
+    rw [EraseIdxAppend.eq.Append_EraseIdx.of.LeLength (by grind)]
+    simp [EraseIdx.eq.Append_Drop_Add_1]
+    simp [show s'.length - 2 + 1 = s'.length - 1 by omega]
+    rw [Drop.eq.ListGet.of.GtLength_0 (by omega)]
+
+
+-- created on 2026-01-07
