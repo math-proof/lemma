4a8844c72739db8aef64cd6a51839b6377f31dd9
cosmosZhou <744984949@qq.com>
2026-07-10 13:28:38 +0800

    new file:   Lemma/Tensor/SEqResize/of/Eq_Get.lean

--- Lemma/Tensor/Matmul/as/BroadcastMatmul/of/LtGetS_SubLength/GeLength_2/GeLength_2.lean
+++ Lemma/Tensor/Matmul/as/BroadcastMatmul/of/LtGetS_SubLength/GeLength_2/GeLength_2.lean
@@ -1,12 +1,12 @@
-import Lemma.Tensor.ResizeCast.as.Resize.of.Eq
-import Lemma.Bool.SEqCastS.of.SEq.Eq.Eq
-import Lemma.Tensor.EqBroadcastMatmulS.of.SEq.SEq
-import Lemma.Tensor.EqBroadcastMatmulS.of.Eq.Eq
-import Lemma.Nat.EqMax.of.Lt
-import Lemma.List.EqAppendTake__ListGet.of.GeLength_2
-import Lemma.Nat.EqAddMulDiv
 import Lemma.Bool.SEq.is.EqCast.of.Eq
-open List Nat Bool Tensor
+import Lemma.Bool.SEq.is.SEqCast.of.Eq
+import Lemma.Bool.SEqCastS.of.SEq.Eq.Eq
+import Lemma.List.EqAppendTake__ListGet.of.GeLength_2
+import Lemma.Nat.EqMax.of.Lt
+import Lemma.Tensor.EqBroadcastMatmulS.of.SEq.SEq
+import Lemma.Tensor.ResizeCast.as.Resize.of.Eq
+import Lemma.Tensor.SEqResize.of.Eq_Get
+open Bool List Nat Tensor
 
 
 @[main, cast]
@@ -32,53 +32,54 @@ private lemma main
 -- proof
   unfold Tensor.matmul
   apply SEq.of.Eq_Cast
-  .
-    split_ifs
-    .
+  ·
+    split_ifs with h h h h h
+    ·
       grind
-    .
+    ·
       grind
-    .
+    ·
       grind
-    .
+    ·
       grind
-    .
+    ·
       grind
-    .
+    ·
       simp
       apply EqBroadcastMatmulS.of.SEq.SEq
-      .
+      ·
         apply SEqCastS.of.SEq.Eq.Eq
-        .
+        ·
           simp
-        .
+        ·
           simp
-        .
+        ·
           rw [EqMax.of.Lt h_n]
-      .
+      ·
         apply SEqCastS.of.SEq.Eq.Eq
-        .
+        ·
           simp
-        .
-          rw [List.EqAppendTake__ListGet.of.GeLength_2 (by grind)]
-        .
+        ·
+          rw [EqAppendTake__ListGet.of.GeLength_2 (by grind)]
+        ·
           rw [EqMax.of.Lt h_n]
           rw [ResizeCast.eq.Cast_Resize.of.Eq]
-          .
+          ·
             simp
             apply SEqCast.of.SEq.Eq
-            .
+            ·
               simp
-              rw [List.EqAppendTake__ListGet.of.GeLength_2 (by grind)]
-            .
-              sorry
-              erw [List.LengthTake.eq.Min_Length]
-          .
-            rw [List.EqAppendTake__ListGet.of.GeLength_2 (by grind)]
-  .
+              rw [EqAppendTake__ListGet.of.GeLength_2 (by grind)]
+            ·
+              apply SEqResize.of.Eq_Get (i := ⟨(s'.take (s'.length - 2)).length, by grind⟩)
+              simp
+          ·
+            rw [EqAppendTake__ListGet.of.GeLength_2 (by grind)]
+  ·
     congr
     simp [Tensor.matmul_shape, Tensor.broadcast_shape]
     grind
 
 
 -- created on 2026-01-10
+-- updated on 2026-07-10
---
+++ Lemma/Tensor/SEqResize/of/Eq_Get.lean
@@ -0,0 +1,20 @@
+import Lemma.Tensor.SEqResize.of.GtLength_0
+open Tensor Bool List Fin
+
+
+@[main, cast]
+private lemma main
+  [Zero α]
+  {s : List ℕ}
+  {i : Fin s.length}
+-- given
+  (h_n : n = s[i])
+  (X : Tensor α s) :
+-- imply
+  X.resize ⟨i, by grind⟩ n ≃ X := by
+-- proof
+  have := SEqResize.of.GtLength_0 (by grind) X (i := i)
+  grind
+
+
+-- created on 2026-07-10
