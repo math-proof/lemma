00cf01734e8d5bda19209d7f003c014bab55829e
cosmosZhou <744984949@qq.com>
2026-07-10 19:17:02 +0800

    modified:   Lemma/Tensor/GetMulCastS/eq/MulGetS.lean

--- Lemma/Tensor/GetMulCastS/eq/MulGetS.lean
+++ Lemma/Tensor/GetMulCastS/eq/MulGetS.lean
@@ -16,7 +16,7 @@ private lemma main
   (j : Fin n)
   (k : Fin l) :
 -- imply
-  (((cast (congrArg (Tensor α) (show ([] ++ [m, 1, l]).set 1 (n * ([] ++ [m, 1, l])[1]) = [] ++ [m, n, l] by grind)) ((A.unsqueeze 1).repeat n (1 : Fin 3)) * cast (congrArg (Tensor α) (show ([] ++ [1, n, l]).set 0 (m * ([] ++ [1, n, l])[0]) = [] ++ [m, n, l] by grind)) (((cast (congrArg (Tensor α) (show ([] ++ [l, n]).swap (([] ++ [l, n]).length - 2) (([] ++ [l, n]).length - 1) = [] ++ [n, l] by simp [EqSwap_0'1])) Bᵀ).unsqueeze 0).repeat m (0 : Fin 3))).get i).get j).get k =
+  (((cast (congrArg (Tensor α) (show ([] ++ [m, 1, l]).set 1 (n * ([] ++ [m, 1, l])[1]) = [] ++ [m, n, l] by grind)) ((A.unsqueeze 1).repeat (1 : Fin 3) n) * cast (congrArg (Tensor α) (show ([] ++ [1, n, l]).set 0 (m * ([] ++ [1, n, l])[0]) = [] ++ [m, n, l] by grind)) (((cast (congrArg (Tensor α) (show ([] ++ [l, n]).swap (([] ++ [l, n]).length - 2) (([] ++ [l, n]).length - 1) = [] ++ [n, l] by simp [EqSwap_0'1])) Bᵀ).unsqueeze 0).repeat (0 : Fin 3) m)).get i).get j).get k =
     (A.get i).get k * (B.get k).get j := by
 -- proof
   erw [GetMul.eq.MulGetS.fin _ _ i]
