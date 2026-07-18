ef0a754b0c80b891db60c8db35d46d49fbe9f2e8
cosmosZhou <744984949@qq.com>
2026-07-18 17:41:53 +0800

    modified:   Lemma/Tensor/SEqDotS/of/SEq.lean

--- Lemma/Tensor/GetDot/eq/DotGet/of/GtLengthS.lean
+++ Lemma/Tensor/GetDot/eq/DotGet/of/GtLengthS.lean
@@ -1,8 +1,13 @@
-import Lemma.List.Ne_Nil.is.GeLength_1
+import Lemma.Bool.SEqCast.of.Eq
+import Lemma.List.EqAppendTake__ListGet.of.GeLength_2
+import Lemma.List.EqAppendTake__ListGet.of.GtLength_0
+import Lemma.Tensor.GetDot.as.DotGet.of.GtLengthS
 import Lemma.Tensor.GetDot.eq.DotGet.of.Ne_Nil
 import Lemma.Tensor.GtLengthDot.of.LeLengthS.Ne_Nil
-open List Tensor
-set_option maxHeartbeats 1000000
+import Lemma.Tensor.Length.of.SEq
+import Lemma.Tensor.SEqDotS.of.SEq
+import Lemma.Tensor.SEqGetS.of.SEq.GtLength
+open Bool List Tensor
 
 
 @[main, fin]
@@ -23,8 +28,24 @@ private lemma main
   | [k'] =>
     apply GetDot.eq.DotGet.of.Ne_Nil h_s
   | s₀ :: s₁ :: tl =>
-    sorry
+    let ys : List ℕ := n' :: s₀ :: s₁ :: tl
+    have hXt := EqAppendTake__ListGet.of.GtLength_0 (show s.length > 0 by grind)
+    let hX := congrArg (n :: ·) hXt
+    let hY := EqAppendTake__ListGet.of.GeLength_2 (show ys.length ≥ 2 by grind)
+    let X' := cast (congrArg (Tensor α) hX.symm) X
+    let Y' := cast (congrArg (Tensor α) hY.symm) Y
+    have h_get := GetDot.as.DotGet.of.GtLengthS (by grind) X' Y' i
+    have hY_seq := (SEqCast.of.Eq hY.symm) Y
+    have hX_seq := (SEqCast.of.Eq hX.symm) X
+    have h_dotY := SEqDotS.of.SEq.left hY_seq X'
+    have h_dotX := SEqDotS.of.SEq hX_seq Y
+    have h_dot := SEq.trans h_dotY h_dotX
+    have h_w := GtLengthDot.of.LeLengthS.Ne_Nil (by grind) (by omega) X Y i
+    have h_lhs := SEqGetS.of.SEq.GtLength (by rwa [← Length.of.SEq h_dot] at h_w) h_dot
+    have h_get_i := SEqGetS.of.SEq.GtLength i.isLt hX_seq
+    have h_rhs := (SEqDotS.of.SEq.left hY_seq (X'[i])).trans (SEqDotS.of.SEq h_get_i Y)
+    exact h_lhs.symm.trans (h_get.trans h_rhs)
 
 
 -- created on 2026-01-04
--- updated on 2026-07-15
+-- updated on 2026-07-18
--- Lemma/Tensor/SEqDotS/of/SEq.lean
+++ Lemma/Tensor/SEqDotS/of/SEq.lean
@@ -21,4 +21,22 @@ private lemma main
   rfl
 
 
+@[main]
+private lemma left
+  [Mul α] [Add α] [Zero α]
+  {A : Tensor α s_A}
+  {B : Tensor α s_B}
+-- given
+  (h : A ≃ B)
+  (X : Tensor α s_X) :
+-- imply
+  X @ A ≃ X @ B := by
+-- proof
+  have h_s := h.left
+  subst h_s
+  have h := Eq.of.SEq h
+  subst h
+  rfl
+
+
 -- created on 2026-01-13
