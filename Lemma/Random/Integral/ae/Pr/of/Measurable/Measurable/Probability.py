f47b8a04f15de149a452e06c235d1217bdd75551
cosmosZhou <744984949@qq.com>
2026-09-12 09:17:29 +0800

    modified:   Lemma/Random/Integral/ae/Pr/of/Measurable/Measurable/Probability.py

--- Lemma/Random/Integral/ae/Pr/of/Measurable/Measurable/Probability.py
+++ Lemma/Random/Integral/ae/Pr/of/Measurable/Measurable/Probability.py
@@ -10,10 +10,8 @@ def apply(self):
 
 @prove(provable=False)
 def prove(Eq):
-
     x, y = Symbol(real=True, random=True)
     Eq << apply(Integral[x.var](Pr(x, y)))
-
 
 
 if __name__ == '__main__':
